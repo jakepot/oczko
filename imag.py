2f8af95acbb9425a4b99d6833ae014481896351d
Jakub <jakub.pyra.1@gmail.com>
2018-05-07 16:31:35 +0200

    wyświetlanie wyniku

--- imag.py
+++ imag.py
@@ -2,31 +2,50 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-fn = "07_g"
+fn = "10_h"
 data = cv2.imread("./all/images/" + fn + ".jpg", 0)
+color = cv2.imread("./all/images/" + fn + ".jpg", 1)
 manual = cv2.imread("./all/manual1/" + fn + ".tif", 0)
 mask = cv2.imread("./all/mask/" + fn + "_mask.tif", 0)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
-kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
 scale = 0.25
 
 data = cv2.resize(data, None, fx=scale, fy=scale)
+
+# sharpen
+kernelsh = np.zeros((9, 9), np.float32)
+kernelsh[4, 4] = 2.0
+boxFilter = np.ones((9, 9), np.float32) / 81.0
+kernelsh = kernelsh - boxFilter
+sharpen = cv2.filter2D(data, -1, kernelsh)
+
 manual = cv2.resize(manual, None, fx=scale, fy=scale)
 mask = cv2.resize(mask, None, fx=scale, fy=scale)
-thresh = cv2.adaptiveThreshold(data, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+color = cv2.resize(color, None, fx=scale, fy=scale)
+datag = cv2.GaussianBlur(data,(3,3),0)
+thresh = cv2.adaptiveThreshold(datag, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 gaussed = cv2.GaussianBlur(thresh, (5, 5), 0)
 canny = cv2.Canny(data, 20, 130)
-opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-opened = opened * (mask/255.0)
+opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+opened = opened * (mask / 255.0)
 
-titles = ['original', 'thresh', 'gaussed', 'opened', 'canny', 'manual']
-images = [data, thresh, gaussed, opened, canny, manual]
+color = color[..., ::-1]
+for row in range(len(color)):
+    for col in range(len(color[row])):
+        if opened[row][col] == 255:
+            color[row][col] = [255, 255, 255]
+
+titles = ['original', 'thresh', 'gaussed', 'opened', 'manual']
+images = [data, thresh, gaussed, opened, manual]
 
 for i in range(len(titles)):
     plt.subplot(2, 3, i + 1)
     plt.imshow(images[i], cmap='gray')
     plt.title(titles[i])
+
+plt.subplot(2, 3, 6)
+plt.imshow(color)
 
 tp = 0
 tn = 0
